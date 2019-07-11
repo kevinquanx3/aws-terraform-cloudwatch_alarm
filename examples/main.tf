04fc13470f01cc82ad3d48c7fafac0856edf2bfc
riko rakhmanto <riko.rakhmanto@rackspace.com>
2019-07-12 09:20:09 +1000

    update examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,33 +1,33 @@
 provider "aws" {
-  version = "~> 1.2"
+  version = "~> 2.0"
   region  = "us-west-2"
 }
 
 module "vpc" {
-  source   = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=v0.0.6"
+  source   = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork?ref=tf_0.12-upgrade"
   vpc_name = "EC2-AR-BaseNetwork-Test1"
 }
 
 module "customer_notifications" {
-  source     = "git@github.com:rackspace-infrastructure-automation/aws-terraform-sns//?ref=v0.0.2"
+  source     = "git@github.com:rackspace-infrastructure-automation/aws-terraform-sns//?ref=tf_0.12-upgrade"
   topic_name = "my-notification-topic"
 }
 
 module "ec2_ar1" {
-  source              = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=v0.0.9"
+  source              = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=tf_0.12-upgrade"
   ec2_os              = "amazon2"
-  subnets             = "${module.vpc.private_subnets}"
-  security_group_list = ["${module.vpc.default_sg}"]
+  subnets             = module.vpc.private_subnets
+  security_group_list = [module.vpc.default_sg]
   instance_type       = "t2.micro"
   resource_name       = "test_amazon"
 }
 
 module "ec2_ar2" {
-  source              = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=v0.0.9"
+  source              = "git@github.com:rackspace-infrastructure-automation/aws-terraform-ec2_autorecovery?ref=tf_0.12-upgrade"
   ec2_os              = "ubuntu16"
   instance_count      = "2"
-  subnets             = "${module.vpc.private_subnets}"
-  security_group_list = ["${module.vpc.default_sg}"]
+  subnets             = module.vpc.private_subnets
+  security_group_list = [module.vpc.default_sg]
   instance_type       = "t2.micro"
   resource_name       = "test_ubuntu"
 }
@@ -36,7 +36,7 @@ module "ec2_ar2" {
 # CWAlarm to create Rackspace Ticket #
 ######################################
 module "ar1_cpu_alarm" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=v0.0.1"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=tf_0.12-upgrade"
 
   alarm_description        = "High CPU Usage on AR1."
   alarm_name               = "CPUAlarmHigh-AR1"
@@ -50,16 +50,18 @@ module "ar1_cpu_alarm" {
   statistic                = "Average"
   threshold                = 90
 
-  dimensions = [{
-    InstanceId = "${element(module.ec2_ar1.ar_instance_id_list, 0)}"
-  }]
+  dimensions = [
+    {
+      InstanceId = element(module.ec2_ar1.ar_instance_id_list, 0)
+    },
+  ]
 }
 
 ##############################
 # CWAlarm to notify customer #
 ##############################
 module "ar1_network_out_alarm" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=v0.0.1"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=tf_0.12-upgrade"
 
   alarm_description       = "High Outbound Network traffic > 1MBps."
   alarm_name              = "NetworkOutAlarmHigh-AR1"
@@ -68,14 +70,16 @@ module "ar1_network_out_alarm" {
   evaluation_periods      = 10
   metric_name             = "NetworkOut"
   namespace               = "AWS/EC2"
-  notification_topic      = ["${module.customer_notifications.topic_arn}"]
+  notification_topic      = [module.customer_notifications.topic_arn]
   period                  = 60
   statistic               = "Average"
   threshold               = 60000000
 
-  dimensions = [{
-    InstanceId = "${element(module.ec2_ar1.ar_instance_id_list, 0)}"
-  }]
+  dimensions = [
+    {
+      InstanceId = element(module.ec2_ar1.ar_instance_id_list, 0)
+    },
+  ]
 }
 
 ########################################
@@ -85,7 +89,7 @@ data "null_data_source" "alarm_dimensions" {
   count = 2
 
   inputs = {
-    InstanceId = "${element(module.ec2_ar2.ar_instance_id_list, count.index)}"
+    InstanceId = element(module.ec2_ar2.ar_instance_id_list, count.index)
     device     = "xvda1"
     fstype     = "ext4"
     path       = "/"
@@ -93,13 +97,13 @@ data "null_data_source" "alarm_dimensions" {
 }
 
 module "ar2_disk_usage_alarm" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=v0.0.1"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=tf_0.12-upgrade"
 
   alarm_count              = "2"
   alarm_description        = "High Disk usage."
   alarm_name               = "HighDiskUsageAlarm-AR2"
   comparison_operator      = "GreaterThanOrEqualToThreshold"
-  dimensions               = "${data.null_data_source.alarm_dimensions.*.outputs}"
+  dimensions               = data.null_data_source.alarm_dimensions.*.outputs
   evaluation_periods       = 30
   metric_name              = "disk_used_percent"
   namespace                = "System/Linux"
